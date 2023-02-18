f23b1969cfe3b0baa849cc1cce24d9b04a70837e
evultrole <evultrole@gmail.com>
2023-02-17 21:26:06 -0700

    Update base.py

--- scripts/base.py
+++ scripts/base.py
@@ -442,9 +442,9 @@ def set_cwd(dir):
 # git ---------------------------------------------------
 def git_update(repo, is_no_errors=False, is_current_dir=False):
   print("[git] update: " + repo)
-  url = "https://github.com/ONLYOFFICE/" + repo + ".git"
+  url = "https://github.com/evultrole/" + repo + ".git"
   if config.option("git-protocol") == "ssh":
-    url = "git@github.com:ONLYOFFICE/" + repo + ".git"
+    url = "git@github.com:evultrole/" + repo + ".git"
   folder = get_script_dir() + "/../../" + repo
   if is_current_dir:
     folder = repo
@@ -514,9 +514,9 @@ def get_branding_repositories(checker):
 
 def create_pull_request(branches_to, repo, is_no_errors=False, is_current_dir=False):
   print("[git] create pull request: " + repo)
-  url = "https://github.com/ONLYOFFICE/" + repo + ".git"
+  url = "https://github.com/evultrole/" + repo + ".git"
   if config.option("git-protocol") == "ssh":
-    url = "git@github.com:ONLYOFFICE/" + repo + ".git"
+    url = "git@github.com:evultrole/" + repo + ".git"
   folder = get_script_dir() + "/../../" + repo
   if is_current_dir:
     folder = repo
